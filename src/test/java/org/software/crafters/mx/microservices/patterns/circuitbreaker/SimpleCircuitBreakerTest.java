d4f02229ba07c87d74fb5d59bdd8de97d6fdea6b
Gerardo Zúñiga <gerardo.blupub@gmail.com>
2021-02-05 21:33:55 -0600

    Red: Was updated the uni test class SimpleCircuitBreakerTest. Was implemented a test that validates that if the wrapped function call failed and the number of failures is under the predetermined threshold, the circuit remains closed.

--- src/test/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreakerTest.java
+++ src/test/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreakerTest.java
@@ -2,9 +2,8 @@ package org.software.crafters.mx.microservices.patterns.circuitbreaker;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.function.Function;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class SimpleCircuitBreakerTest {
 
@@ -12,8 +11,34 @@ public class SimpleCircuitBreakerTest {
     public void callFunctionSuccessfully() {
         System.out.println("Test - Call function successfully [Closed status]");
         SimpleCircuitBreaker<String, String> circuitBreaker =
-                new SimpleCircuitBreaker(name -> { return String.format("Hello %s!", name);});
+                new SimpleCircuitBreaker(name -> {
+                    return String.format("Hello %s!", name);
+                });
         assertEquals("Hello Gerardo!", circuitBreaker.call("Gerardo"));
         assertEquals(SimpleCircuitBreaker.Status.CLOSED, circuitBreaker.getStatus());
+    }
+
+    @Test
+    public void functionCallFailedAndIsUnderFailureThreshold() {
+        System.out.println("Test - Function call failed and is under the failure threshold [Closed status]");
+        int failureThreshold = 3;
+        SimpleCircuitBreaker<String, String> circuitBreaker = new SimpleCircuitBreaker(name -> {
+            if (name != null) {
+                return String.format("Hello %s!", name);
+            }
+            throw new IllegalArgumentException("Invalid argument: name cannot be null");
+        },
+        failureThreshold);
+
+        int failureCounter = 0;
+        while(failureCounter < circuitBreaker.getFailureThreshold() - 1) {
+            try {
+                circuitBreaker.call(null);
+            } catch (IllegalArgumentException) {
+                failureCounter++;
+            }
+        }
+        assertEquals(circuitBreaker.getFailureThreshold() - 1, failureCounter);
+
     }
 }
