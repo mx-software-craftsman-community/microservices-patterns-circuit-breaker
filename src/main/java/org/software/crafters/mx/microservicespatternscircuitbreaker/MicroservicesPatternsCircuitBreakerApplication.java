94c65d55bf277adacec7d53e42a76271c1d81b01
Gerardo Zúñiga <gerardo.blupub@gmail.com>
2021-02-05 19:21:04 -0600

    Project initialized with Spring Initializr

---
+++ src/main/java/org/software/crafters/mx/microservicespatternscircuitbreaker/MicroservicesPatternsCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package org.software.crafters.mx.microservicespatternscircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPatternsCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesPatternsCircuitBreakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/software/crafters/mx/microservicespatternscircuitbreaker/MicroservicesPatternsCircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.software.crafters.mx.microservicespatternscircuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesPatternsCircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
