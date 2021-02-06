43b4a013de241f6ade96c3286952bee87d05b433
Gerardo Zúñiga <gerardo.blupub@gmail.com>
2021-02-05 20:51:14 -0600

    Green: Was updated the class SimpleCircuitBreaker to add the most simple code to pass the unit test that validates a successful call to the protected function and a CLOSED status in the circuit breaker.

--- src/main/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreaker.java
+++ src/main/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreaker.java
@@ -1,7 +1,17 @@
 package org.software.crafters.mx.microservices.patterns.circuitbreaker;
 
-public class SimpleCircuitBreaker {
-    public void execute() {
+import java.util.function.Function;
+
+public class SimpleCircuitBreaker<T, R> {
+
+    private final Function<T, R> protectedFunction;
+
+    public SimpleCircuitBreaker(Function<T, R> protectedFunction) {
+        this.protectedFunction = protectedFunction;
+    }
+
+    public R call(T arg) {
+        return protectedFunction.apply(arg);
     }
 
     public Status getStatus() {
--- src/test/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreakerTest.java
+++ src/test/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreakerTest.java
@@ -10,6 +10,7 @@ public class SimpleCircuitBreakerTest {
 
     @Test
     public void callFunctionSuccessfully() {
+        System.out.println("Test - Call function successfully [Closed status]");
         SimpleCircuitBreaker<String, String> circuitBreaker =
                 new SimpleCircuitBreaker(name -> { return String.format("Hello %s!", name);});
         assertEquals("Hello Gerardo!", circuitBreaker.call("Gerardo"));
