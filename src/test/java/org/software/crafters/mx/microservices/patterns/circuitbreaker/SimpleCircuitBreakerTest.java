f83dd0e56f350821418408277c861ea2c7a7ad44
Gerardo Zúñiga <gerardo.blupub@gmail.com>
2021-02-05 20:32:54 -0600

    Refactor(Red): Was renamed the test method SimpleCircuitBreakerTest.executeFunctionCallSuccessfully as SimpleCircuitBreakerTest.executeFunctionCallSuccessfully. Also, the test method was improved to add the code for validating a successful call to the protected function.

--- src/test/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreakerTest.java
+++ src/test/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreakerTest.java
@@ -2,14 +2,17 @@ package org.software.crafters.mx.microservices.patterns.circuitbreaker;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Function;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleCircuitBreakerTest {
 
     @Test
     public void executeFunctionCallSuccessfully() {
-        SimpleCircuitBreaker circuitBreaker = new SimpleCircuitBreaker();
-        circuitBreaker.execute();
+        SimpleCircuitBreaker<String, String> circuitBreaker =
+                new SimpleCircuitBreaker(name -> { return String.format("Hello %s!", name);});
+        assertEquals("Hello Gerardo!", circuitBreaker.execute("Gerardo"));
         assertEquals(SimpleCircuitBreaker.Status.CLOSED, circuitBreaker.getStatus());
     }
 }
