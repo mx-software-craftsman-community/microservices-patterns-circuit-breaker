ebde24cc6f94970e07ef31201bda3bb7347f4777
Gerardo Zúñiga <gerardo.blupub@gmail.com>
2021-02-05 19:27:29 -0600

    Was renamed tha application wass as Application.

--- src/main/java/org/software/crafters/mx/microservicespatternscircuitbreaker/Application.java
+++ src/main/java/org/software/crafters/mx/microservicespatternscircuitbreaker/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroservicesPatternsCircuitBreakerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroservicesPatternsCircuitBreakerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/org/software/crafters/mx/microservicespatternscircuitbreaker/ApplicationTests.java
+++ src/test/java/org/software/crafters/mx/microservicespatternscircuitbreaker/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroservicesPatternsCircuitBreakerApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
