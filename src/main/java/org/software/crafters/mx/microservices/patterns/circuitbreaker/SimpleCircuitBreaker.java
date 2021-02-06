91bb4ac43031d7c1fdc1fc43dc8d590629ee45a1
Gerardo Zúñiga <gerardo.blupub@gmail.com>
2021-02-06 17:22:38 -0600

    Refactor: Was improved the class Simple Circuit Breaker to maintain the failure count and throw an error when the function call failed and the failure count is under the threshold. The tests were updated conform to the previous changes.

--- src/main/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreaker.java
+++ src/main/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreaker.java
@@ -6,19 +6,25 @@ public class SimpleCircuitBreaker<T, R> {
 
     private final Function<T, R> protectedFunction;
     private final int failureThreshold;
+    private int failureCount;
 
     public SimpleCircuitBreaker(Function<T, R> protectedFunction) {
-        this.protectedFunction = protectedFunction;
-        this.failureThreshold = 1;
+        this(protectedFunction, 1);
     }
 
     public SimpleCircuitBreaker(Function<T, R> protectedFunction, int failureThreshold) {
         this.protectedFunction = protectedFunction;
         this.failureThreshold = failureThreshold;
+        this.failureCount = 0;
     }
 
-    public R call(T arg) {
-        return protectedFunction.apply(arg);
+    public R call(T arg) throws Exception {
+        try {
+            return protectedFunction.apply(arg);
+        } catch (Exception e) {
+            failureCount++;
+            throw e;
+        }
     }
 
     public Status getStatus() {
@@ -27,6 +33,10 @@ public class SimpleCircuitBreaker<T, R> {
 
     public int getFailureThreshold() {
         return failureThreshold;
+    }
+
+    public int getFailureCount() {
+        return failureCount;
     }
 
     public enum Status {CLOSED}
--- src/test/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreakerTest.java
+++ src/test/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreakerTest.java
@@ -3,12 +3,11 @@ package org.software.crafters.mx.microservices.patterns.circuitbreaker;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class SimpleCircuitBreakerTest {
 
     @Test
-    public void callFunctionSuccessfully() {
+    public void callFunctionSuccessfully() throws Exception {
         System.out.println("Test - Call function successfully [Closed status]");
         SimpleCircuitBreaker<String, String> circuitBreaker =
                 new SimpleCircuitBreaker(name -> {
@@ -22,23 +21,24 @@ public class SimpleCircuitBreakerTest {
     public void functionCallFailedAndIsUnderFailureThreshold() {
         System.out.println("Test - Function call failed and is under the failure threshold [Closed status]");
         int failureThreshold = 3;
-        SimpleCircuitBreaker<String, String> circuitBreaker = new SimpleCircuitBreaker(name -> {
-            if (name != null) {
-                return String.format("Hello %s!", name);
-            }
-            throw new IllegalArgumentException("Invalid argument: name cannot be null");
-        },
-        failureThreshold);
+        SimpleCircuitBreaker<String, String> circuitBreaker = new SimpleCircuitBreaker(
+                name -> {
+                    if (name != null) {
+                        return String.format("Hello %s!", name);
+                    }
+                    throw new IllegalArgumentException("Invalid argument: name cannot be null");
+                },
+                failureThreshold);
 
-        int failureCounter = 0;
-        while(failureCounter < circuitBreaker.getFailureThreshold() - 1) {
+        while (circuitBreaker.getFailureCount() < circuitBreaker.getFailureThreshold() - 1) {
             try {
                 circuitBreaker.call(null);
-            } catch (IllegalArgumentException e) {
-                failureCounter++;
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
             }
         }
-        assertEquals(circuitBreaker.getFailureThreshold() - 1, failureCounter);
 
+        assertEquals(circuitBreaker.getFailureThreshold() - 1, circuitBreaker.getFailureCount());
+        assertEquals(SimpleCircuitBreaker.Status.CLOSED, circuitBreaker.getStatus());
     }
 }
