e70c14886d6c3a556abe281c440175931e87cc00
Gerardo Zúñiga <gerardo.blupub@gmail.com>
2021-02-08 17:58:23 -0600

    Green: Was updated the unit test class SimpleCircuitBreaker to add the most simple code to pass the unit test that validates that if the wrapped function call failed and the number of failures reaches the predetermined threshold, then and error is returned, the Circuit Breaker trips and it goes to the open state.

--- src/main/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreaker.java
+++ src/main/java/org/software/crafters/mx/microservices/patterns/circuitbreaker/SimpleCircuitBreaker.java
@@ -5,6 +5,7 @@ import java.util.function.Function;
 public class SimpleCircuitBreaker<T, R> {
 
     private final Function<T, R> protectedFunction;
+    private Status status;
     private final int failureThreshold;
     private int failureCount;
 
@@ -14,6 +15,7 @@ public class SimpleCircuitBreaker<T, R> {
 
     public SimpleCircuitBreaker(Function<T, R> protectedFunction, int failureThreshold) {
         this.protectedFunction = protectedFunction;
+        this.status = Status.CLOSED;
         this.failureThreshold = failureThreshold;
         this.failureCount = 0;
     }
@@ -23,12 +25,23 @@ public class SimpleCircuitBreaker<T, R> {
             return protectedFunction.apply(arg);
         } catch (Exception e) {
             failureCount++;
+            if(failureCountReachesThreshold()) {
+                trip();
+            }
             throw e;
         }
     }
 
+    private boolean failureCountReachesThreshold() {
+        return failureCount == failureThreshold;
+    }
+
+    private void trip() {
+        this.status = Status.OPEN;
+    }
+
     public Status getStatus() {
-        return Status.CLOSED;
+        return status;
     }
 
     public int getFailureThreshold() {
